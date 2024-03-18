076df8ce321cda410d95d7e153f912116e8b0608
EnricoPezzano <enrico.pezzano@outlook.it>
2024-03-18 14:55:19 +0100

    enrico's script push

--- my-rsync.sh
+++ my-rsync.sh
@@ -15,7 +15,7 @@ rsync -avh --delete "$source" "$destination" | grep 'err\|delete'
 
 source=${h}/Music/
 destination=${h}/Library/Mobile\ Documents/com~apple~CloudDocs/Music/
-rsync -avh --delete --exclude 'Music' "$source" "$destination" | grep 'err\|delete'
+rsync -avh --delete --exclude Music "$source" "$destination" | grep 'err\|delete'
 
 cd $HOME/Desktop/AutoMac; echo "\033[1m \n-> $PWD \033[0m"; +mgp
 cd $HOME/Desktop/calvino-git; echo "\033[1m \n-> $PWD \033[0m"; +mgp
