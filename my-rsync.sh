1fe55bd26a79498a1f8f823f8386852ccd577d0c
EnricoPezzano <enrico.pezzano@outlook.it>
2024-07-04 14:55:11 +0200

    enrico's script push

--- my-rsync.sh
+++ my-rsync.sh
@@ -2,6 +2,7 @@
 
 smartctl -a disk0 | grep "Data Units Written"
 
+echo "Rsyncing desktop..."
 h=$HOME
 source=${h}/Desktop/
 destination=${h}/Library/Mobile\ Documents/com~apple~CloudDocs/Desktop/
@@ -9,12 +10,17 @@ destination=${h}/Library/Mobile\ Documents/com~apple~CloudDocs/Desktop/
 excluded_folders=( 'unige-git' 'miscellaneous*' 'iPhone*')
 rsync -avh --exclude "${excluded_folders[0]}" --exclude "${excluded_folders[1]}" --exclude "${excluded_folders[2]}" --delete "$source" "$destination" | grep 'err\|delete'
 
+echo "Rsyncing documents..."
 source=${h}/Documents/
 destination=${h}/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/
 rsync -avh --delete "$source" "$destination" | grep 'err\|delete'
 
 source=${h}/Music/
 destination=${h}/Library/Mobile\ Documents/com~apple~CloudDocs/Music/
+rsync -avh --delete --exclude Music "$source" "$destination" | grep 'err\|delete'
+
+source=${h}/Downloads/
+destination=${h}/Library/Mobile\ Documents/com~apple~CloudDocs/Downloads/
 rsync -avh --delete --exclude Music "$source" "$destination" | grep 'err\|delete'
 
 # cd $HOME/Desktop/AutoMac; echo "\033[1m \n-> $PWD \033[0m"; +mgp
