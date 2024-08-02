00cac6b55d38df6c4855f05caabab4e4aca19697
EnricoPezzano <enrico.pezzano@outlook.it>
2024-08-02 03:02:30 +0200

    enrico's script push

--- my-git-push.sh
+++ my-git-push.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/zsh
 
+echo Pulling...
 git pull
 
+echo 
+echo Adding...
 git add *
 
 if [ $# -eq 0 ]
     then
         msg="enrico's script push"
     else
-        msg=$1
+        msg=$*
 fi
 
+echo
+echo Committing...$msg
 git commit -am "$msg"
 
 # echo 'Enter the name of the branch:'
 # read branch
 
+echo
+echo Pushing...
 git push #origin $branch
