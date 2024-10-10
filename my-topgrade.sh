ce3d6c4794959f48b71b6e6930d2c053dc3ebfd2
EnricoPezzano <enrico.pezzano@outlook.it>
2024-10-11 00:02:55 +0200

    rsync

--- my-topgrade.sh
+++ my-topgrade.sh
@@ -2,6 +2,6 @@
 
 for i in {1..50}; do ping -c1 www.google.com &> /dev/null && break; done
 
-topgrade -y --disable system
+yes | topgrade -y --disable system
 
 osascript -e 'tell application "Terminal" to quit'
